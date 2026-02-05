63aa9117e18d08e9d927fbd4587515bca96534d9
Valentin Marchessi <vmarchessi@gmail.com>
2026-02-05 17:11:36 -0300

    Add Facebook Pixel tracking to layout for enhanced analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,29 @@ export default function RootLayout({
           `,
           }}
         />
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '980582796353909');
+          fbq('track', 'PageView');`
+          }}
+        />
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=980582796353909&ev=PageView&noscript=1"
+          />`}}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-uk-blue-bg`}
